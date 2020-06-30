8d1cc236e0312029ffcf5f87244501688766baa7
Tiến Nguyễn Hữu <tiennhse140275@fpt.edu.vn>
2020-06-30 10:50:37 +0700

    .

--- week2-md/js/app.js
+++ week2-md/js/app.js
@@ -5,7 +5,7 @@
 
     // Pass in the filter service
     // for the ability to use it
-    Wk2Controller.$inject = [$scope,$filter];
+    Wk2Controller.$inject = ['$scope','$filter'];
 
     function Wk2Controller($scope, $filter){
         // You can use the $filter service with the following syntax:
@@ -15,6 +15,9 @@
         // {{varname | filterType [: option1 : option2]}}
         $scope.header = $filter('lowercase')('GREETINGS');
 
+        $scope.totalPrice = 0;
+
+        $scope.itemAmount = [0,0,0,0,0,0];
         $scope.price = getRandomPrices();
         function getRandomPrices(){
             let price = [];
@@ -24,5 +27,10 @@
             
             return price;
         }
+        $scope.addItem = function(itemIndex){
+            $scope.totalPrice += $scope.price[itemIndex];
+            $scope.itemAmount[itemIndex] += 1;
+            document.getElementsByClassName("food-item-count")[itemIndex].classList.remove('hide-item');
+        };
     }
 })();
